20a991f7f4f50802b25a535916994572c718ace1
Shahan Neda <shahan.neda@gmail.com>
2019-10-28 17:25:06 -0700

    got keyboard input to work

--- src/components/type.js
+++ src/components/type.js
@@ -2,18 +2,39 @@ import React from 'react';
 class TypeComponent extends React.Component{
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {screenText:"Type this text and it will help you get faster!"};
   }
   componentDidMount(){
+    document.addEventListener("keydown", this._handleKeyDown);
+
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this._handleKeyDown);
+  }
+  _handleKeyDown(event){
+    switch( event.keyCode ) {
+        case 32:
+            console.log("test");
+            break;
+        default:
+        var characterTyped = String.fromCharCode(event.keyCode);
+        if(!event.shiftKey){
+          characterTyped = characterTyped.toLowerCase();
+        }
+        console.log(characterTyped);
+            break;
+    }
+  }
+  moveCurrentLetterToNextLetter(){
 
   }
   render(){
 
     return <div>
-
-     test, {this.props.name}
-
-     </div>;
+      <div className="text">
+          {this.state.currentLetter} {this.state.screenText}
+      </div>
+    </div>;
   }
 }
 export default TypeComponent;
