9bed70bf9db68d79736ad0ebf09d8f1dea31afd9
shahanneda <shahan.neda@gmail.com>
2021-05-17 20:25:25 -0700

    handeled back of level complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React from 'React'
 import LevelPicker from "./LevelPicker.jsx";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import TypingPage from './TypingPage.jsx';
 import HeaderComponent from './HeaderComponent'
 import SettingsComponent from './SettingComponent.jsx'
@@ -15,11 +15,29 @@ class App extends React.Component {
                 minWPM: 15,
                 darkMode: true,
             },
+            shouldRedirect: false,
+            redirectLocation: "",
         };
 
         this.handleOptionChange = this.handleOptionChange.bind(this);
         this.handleSettingsChange = this.handleSettingsChange.bind(this);
         this.handleDarkMode = this.handleDarkMode.bind(this);
+        this.handleNextLevel = this.handleNextLevel.bind(this);
+    }
+
+    handleNextLevel(currentLevelName){
+        // find level so we can get its index and incremenit it
+        let index = this.props.differentTexts.findIndex(textObj => textObj.title == currentLevelName);
+        if(index +1 >= this.props.differentTexts.length || index == -1){
+            index = 0;
+        }else{
+            index = index +1;
+        }
+
+        this.setState({
+            shouldRedirect: true,
+            redirectLocation: "/type/" + this.props.differentTexts[index].title
+        })
     }
 
     render() {
@@ -29,15 +47,17 @@ class App extends React.Component {
         if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
             console.warn("Some features not currently supported in Firefox, please switch to chrome!");
         }
+
         return (
             <Router>
+                {this.state.shouldRedirect ? <Redirect to={this.state.redirectLocation} /> : <></>}
 
                 <Switch>
                     <Route path="/type/:text" render={((routeProps) =>
                         <div>
 
                             <HeaderComponent typingPage={true} defaultSettings={this.state.settings} handleSettingsChange={this.handleSettingsChange}  handleDarkMode={this.handleDarkMode}/>
-                            <TypingPage differentTexts={this.props.differentTexts} nameOfLevel={routeProps.match.params.text} routeProps={routeProps} settings={this.state.settings} />
+                            <TypingPage differentTexts={this.props.differentTexts} nameOfLevel={routeProps.match.params.text} routeProps={routeProps} settings={this.state.settings} handleNextLevel={() => this.handleNextLevel(routeProps.match.params.text)} />
 
                         </div>
                     )} />
--- src/components/typingPage.jsx
+++ src/components/typingPage.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import SettingsComponent from "./SettingComponent.jsx"
 import TypeComponent from './Type.jsx';
-import { Link, Redirect } from "react-router-dom"
+import { Link, Redirect, withRouter } from "react-router-dom"
+import LevelComplete from './LevelComplete.jsx';
 export class TypingPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             indexSelected: 0,
         };
+
+        this.handleLevelComplete = this.handleLevelComplete.bind(this);
+        this.handleBack = this.handleBack.bind(this);
     }
 
+
+    handleLevelComplete(){
+
+    }
+
+
+    handleBack()  {
+        this.props.history.push('/browse')
+    }
 
     render() {
         var indexOfText = 0;
@@ -28,7 +41,8 @@ export class TypingPage extends Component {
                 <div key={this.props.text} >
 
                     <div className="outer-text-container">
-                        <TypeComponent align="justify" settings={this.props.settings} text={this.props.differentTexts[indexOfText].text} />
+                        <TypeComponent align="justify" handleLevelComplete={this.handleLevelComplete} settings={this.props.settings} text={this.props.differentTexts[indexOfText].text} />
+                        <LevelComplete handleNext={this.props.handleNextLevel} handleBack={this.handleBack} />
                     </div>
                 </div>
             </div>
@@ -36,4 +50,4 @@ export class TypingPage extends Component {
     }
 }
 
-export default TypingPage;
+export default withRouter(TypingPage);
